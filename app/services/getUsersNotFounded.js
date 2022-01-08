05d62f6fd807c5cbf3c87decf5b205a87758502c
julinuzzo19 <julinuzzo19@gmail.com>
2022-01-08 00:25:20 -0300

    external api calls concurrency

--- app/services/getUsersNotFounded.js
+++ app/services/getUsersNotFounded.js
@@ -2,23 +2,22 @@ const User = require('../models/user');
 const axios = require('axios').default;
 
 module.exports = getUsersApi = async (ids) => {
-  Promise.all(
-    ids.map(async (id) => {
-      await axios
-        .get(`https://reqres.in/api/users/${id}`)
-        .then((res) => {
-          if (res) {
-            const {email, first_name, last_name} = res.data.data;
+  const requests = ids.map((id) => axios.get(`https://reqres.in/api/users/${id}`));
 
-            new User({
-              id,
-              email,
-              first_name,
-              last_name
-            }).save();
-          }
-        })
-        .catch((err) => console.log(err.message));
-    })
-  );
+  try {
+    const result = await Promise.all(requests);
+
+    result.map((res) => {
+      const {id, email, first_name, last_name} = res.data.data;
+
+      new User({
+        id,
+        email,
+        first_name,
+        last_name
+      }).save();
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };

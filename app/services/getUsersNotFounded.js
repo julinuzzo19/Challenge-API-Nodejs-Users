f542702f154c80fe9032e96b6132bb1a774ecd17
julinuzzo19 <julinuzzo19@gmail.com>
2022-01-17 19:31:13 -0300

    changed promise.all for .allSettled

--- app/services/getUsersNotFounded.js
+++ app/services/getUsersNotFounded.js
@@ -5,17 +5,19 @@ module.exports = getUsersApi = async (ids) => {
   const requests = ids.map((id) => axios.get(`https://reqres.in/api/users/${id}`));
 
   try {
-    const result = await Promise.all(requests);
+    const result = await Promise.allSettled(requests);
 
     result.map((res) => {
-      const {id, email, first_name, last_name} = res.data.data;
+      if (res.status === 'fulfilled') {
+        const {id, email, first_name, last_name} = res.value.data.data;
 
-      new User({
-        id,
-        email,
-        first_name,
-        last_name
-      }).save();
+        new User({
+          id,
+          email,
+          first_name,
+          last_name
+        }).save();
+      }
     });
   } catch (error) {
     console.log(error);
